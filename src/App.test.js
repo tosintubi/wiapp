a2632b3bfd6da1f73678b2d044cc506fedf290a7
Tosin <tosintubi@users.noreply.github.com>
2021-09-25 10:37:36 +0200

    changed test renders

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/ welcome to wikiapp!/i);
+  const linkElement = screen.getByText(/welcome to wikiapp!/i);
   expect(linkElement).toBeInTheDocument();
 });
