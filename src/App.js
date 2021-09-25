d71d06aa5500ce043e96016fd134e7771755c1b7
Tosin <tosintubi@users.noreply.github.com>
2021-09-25 10:27:18 +0200

    removed image from homepage

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey There!
+          Welcome to wikiapp!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
